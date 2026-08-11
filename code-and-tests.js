b4d5ce3e1653cfba078d5c40459628a75d1fd1c5
Rayssa Roberta <116031703+rayssarrsilva@users.noreply.github.com>
2026-08-11 17:54:14 -0300

    Create two manual tests (TDD) before the function

---
+++ code-and-tests.js
@@ -0,0 +1,17 @@
+if (orderTotal({
+        items: [
+            {name: 'Dragon food', price: 8},
+            {name: 'Dragon cage (small)', price: 800}
+        ]
+    }) !== 800){
+    throw new Error('Check fail: Happy path')
+}
+
+if (orderTotal({
+    items: [
+        {name: 'Dragon collar', price: 20},
+        {name: 'Dragon chew toy', price: 40}
+    ]
+}) !== 60){
+    throw new Error('check fail: happy path IExample2)')
+}
