cc33f0621afa042866d7a378aabc4d375b436af9
Rayssa Roberta <116031703+rayssarrsilva@users.noreply.github.com>
2026-08-12 16:56:18 -0300

    Create the object calc so the existent test pass

--- odin/func/calculator.js
+++ odin/func/calculator.js
@@ -1,9 +1,17 @@
 const calculator = {
     add(value1, value2){
-        return value1 + value2;
+        const add = value1 + value2;
+        return Number.isInteger(add) ? parseFloat(add.toString()) : parseFloat(add.toFixed(1));
     },
     subtract(value1, value2){
-        return value1 - value2;
+        const subtract = value1 - value2;
+        return Number.isInteger(subtract) ? parseFloat(subtract.toString()) : parseFloat(subtract.toFixed(1));
+    },
+    divide(value1, value2){
+        return parseFloat((value1 / value2).toFixed(2));
+    },
+    multiply(value1, value2){
+        return (value1 * value2);
     }
 };
 
--- odin/test/calculator.test.js
+++ odin/test/calculator.test.js
@@ -1,15 +1,15 @@
 import calculator from "../func/calculator.js";
 
 test('sum two values', () => {
-    expect(calculator.sum(5, 9)).toBe(14);
+    expect(calculator.add(5, 9)).toBe(14);
 });
 
 test('sum two float values', () => {
-    expect(calculator.sum(5.4, 9.3)).toBe(14.7);
+    expect(calculator.add(5.4, 9.3)).toBe(14.7);
 });
 
 test('subtract two float values to negative', () => {
-    expect(calculator.subtract(5.4, 9.3)).toBe(-14.7);
+    expect(calculator.subtract(5.4, 9.3)).toBe(-3.9);
 });
 
 test('subtract two values to negative', () => {
@@ -24,11 +24,11 @@ test('divide two values', () => {
     expect(calculator.divide(25, 5)).toBe(5);
 });
 
-test('divide two float values and round', () => {
+test('divide two float values and return only two values after comma', () => {
     expect(calculator.divide(25.5, 5.7)).toBe(4.47);
 });
 
-test('multiply two float values and round', () => {
+test('multiply two float values and return only two values after comma', () => {
     expect(calculator.multiply(7.1, 5.7)).toBe(40.47);
 });
 
