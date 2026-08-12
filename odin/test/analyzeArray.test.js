26c580620b708c983c79501c166ce8de22421054
Rayssa Roberta <116031703+rayssarrsilva@users.noreply.github.com>
2026-08-12 17:44:39 -0300

    Create another test function that uses negative

--- odin/test/analyzeArray.test.js
+++ odin/test/analyzeArray.test.js
@@ -1,3 +1,5 @@
+import analyzeArray from "../func/analyzeArray.js";
+
 test('return an object with average, min, max, length', () => {
     expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
         average: 4,
@@ -5,4 +7,14 @@ test('return an object with average, min, max, length', () => {
         max: 8,
         length: 6
     });
+});
+
+test('works with negative numbers', () => {
+  const result = analyzeArray([-10, 0, 10]);
+  expect(result).toEqual({
+    average: 0,
+    min: -10,
+    max: 10,
+    length: 3
+  });
 });
