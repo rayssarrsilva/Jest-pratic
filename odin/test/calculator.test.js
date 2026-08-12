b6f5d1dcf0aa85798317eab3b2f4bbc5b4b184da
Rayssa Roberta <116031703+rayssarrsilva@users.noreply.github.com>
2026-08-12 16:04:56 -0300

    Correct some parts of the calculator subtract test

--- odin/test/calculator.test.js
+++ odin/test/calculator.test.js
@@ -6,12 +6,14 @@ test('sum two float values', () => {
     expect(calculator.sum(5.4, 9.3)).toBe(14.7);
 });
 
-test('subtract two float values', () => {
-    expect(calculator.sum(5.4, 9.3)).toBe(14.7);
+test('subtract two float values to negative', () => {
+    expect(calculator.subtract(5.4, 9.3)).toBe(-14.7);
+});
+
+test('subtract two values to negative', () => {
+    expect(calculator.subtract(5, 9)).toBe(-4);
 });
 
 test('subtract two values', () => {
-    expect(calculator.sum(5, 9)).toBe(14);
+    expect(calculator.subtract(25, 5)).toBe(20);
 });
-
-
