cd1566f5857eb4ae8dde0ba42961a5440cf82323
Rayssa Roberta <116031703+rayssarrsilva@users.noreply.github.com>
2026-08-12 17:36:19 -0300

    Create the analyzeArray test function

---
+++ odin/func/analyzeArray.js
@@ -0,0 +1,8 @@
+test('return an object with average, min, max, length', () => {
+    expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+        average: 4,
+        min: 1,
+        max: 8,
+        length: 6
+    });
+});
